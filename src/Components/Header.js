5d8f6f411d8cc1c06cd69ad75035b094f9c0c53d
Aditya Pandey <aditya.adityapandey02@gmail.com>
2020-09-16 16:52:53 +0530

    changing title and adding icon

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -35,7 +35,7 @@ class Header extends Component {
       <div className="row banner">
          <div className="banner-text">
             <h1 className="responsive-headline">I'm {name}.</h1>
-            <h3><span>{description}.</span></h3>
+            <h3><span>{description}</span></h3>
             <hr />
             <ul className="social">
                {networks}
