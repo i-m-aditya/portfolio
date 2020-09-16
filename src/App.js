17c1572cf350cdb54a921f3f0538cea6838b9daf
Aditya Pandey <aditya.adityapandey02@gmail.com>
2020-09-16 13:46:27 +0530

    removing unused variables

--- src/App.js
+++ src/App.js
@@ -4,10 +4,8 @@ import $ from 'jquery';
 import './App.css';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
-import About from './Components/About';
 import Resume from './Components/Resume';
 import Contact from './Components/Contact';
-import Testimonials from './Components/Testimonials';
 import Portfolio from './Components/Portfolio';
 
 class App extends Component {
--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
 
 const Contact = ({ data }) => {
-   const [url, setUrl] = useState('mailto:test@example.com?subject=subject&body=body');
+   // const [url, setUrl] = useState('mailto:test@example.com?subject=subject&body=body');
    const [name, setName] = useState('');
    const [subject, setSubject] = useState('');
-   const [email, setEmail] = useState('prblmslvr.aditya@gmail.com');
+   const email = "prblmslvr.adity@gmail.com"
    const [message, setMessage] = useState('');
 
    console.log(data)
